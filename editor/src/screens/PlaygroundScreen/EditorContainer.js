e914a174d2cf437c95491729537ed7f6c1f7c70d
swapnilgupta14 <swapnilguptaswag@gmail.com>
2024-10-19 14:04:18 +0530

    fix: editor

--- editor/src/screens/PlaygroundScreen/EditorContainer.js
+++ editor/src/screens/PlaygroundScreen/EditorContainer.js
@@ -1,59 +1,64 @@
-import "./EditorContainer.scss"
-import Editor from "@monaco-editor/react"
-
+import "./EditorContainer.scss";
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
 
 export const EditorContainer = () => {
-  return(
+  const [code, setCode] = useState("dwehd ");
+  console.log(code)
+
+  return (
     <div className="root-editor-container">
-        <div className="editor-header">
-          <div className="editor-left-container">
-            <b className="title">{"Title of the Card"}</b>
-            <span className="material-icons">edit</span>
-            <button>Save Code</button>
-          </div>
-          <div className="editor-right-container">
-            <select>
-              <option value="cpp">CPP</option>
-              <option value="javascript">JavaScript</option>
-              <option value="java">Java</option>
-              <option value="python">Python</option>
-            </select>
-            <select>
-              <option value="vs-dark">Vs-Dark</option>
-              <option value="vs-light">Vs-Light</option>
-            </select>
-          </div>
+      <div className="editor-header">
+        <div className="editor-left-container">
+          <b className="title">{"Title of the Card"}</b>
+          <span className="material-icons">edit</span>
+          <button>Save Code</button>
         </div>
-        <div className="editor-body">
-            <Editor 
-                width={200}
-                height={200}
-                options={{
-                  readOnly: false, // Ensure this is set to false
-                  selectOnLineNumbers: true,
-                }}
-            />\
-            
+        <div className="editor-right-container">
+          <select>
+            <option value="cpp">CPP</option>
+            <option value="javascript">JavaScript</option>
+            <option value="java">Java</option>
+            <option value="python">Python</option>
+          </select>
+          <select>
+            <option value="vs-dark">Vs-Dark</option>
+            <option value="vs-light">Vs-Light</option>
+          </select>
         </div>
-        <div className="editor-footer">
-            <button className="btn">
-                <span className="material-icons">fullscreen</span>
-                <span>Full Screen</span>
-            </button>
-            <label htmlFor="import-code" className="btn">
-                <span className="material-icons">cloud_upload</span>
-                <span>Import Code</span>
-            </label>
-            <input type="file" id="import-code" style={{display: 'none'}} />
-            <button className="btn">
-                <span className="material-icons">cloud_download</span>
-                <span>Export Code</span>
-            </button>
-            <button className="btn">
-                <span className="material-icons">play_arrow</span>
-                <span>Run Code</span>
-            </button>
-        </div>
+      </div>
+      <div className="editor-body">
+        <Editor
+          value={code}
+          onChange={(e) => setCode(e)}
+          width={900}
+          height={900}
+          options={{
+            readOnly: false, // Ensure this is set to false
+            selectOnLineNumbers: true,
+          }}
+        />
+        \
+      </div>
+      <div className="editor-footer">
+        <button className="btn">
+          <span className="material-icons">fullscreen</span>
+          <span>Full Screen</span>
+        </button>
+        <label htmlFor="import-code" className="btn">
+          <span className="material-icons">cloud_upload</span>
+          <span>Import Code</span>
+        </label>
+        <input type="file" id="import-code" style={{ display: "none" }} />
+        <button className="btn">
+          <span className="material-icons">cloud_download</span>
+          <span>Export Code</span>
+        </button>
+        <button className="btn">
+          <span className="material-icons">play_arrow</span>
+          <span>Run Code</span>
+        </button>
+      </div>
     </div>
   );
-}
+};
