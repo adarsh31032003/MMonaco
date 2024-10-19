1603d0e95eb22e5c81d75fd28523d7ec354a1ecb
adarsh31032003 <tandon.adarsh2003@gmail.com>
2024-10-19 10:37:41 +0530

    Add editor files

---
+++ editor/src/screens/PlaygroundScreen/index.js
@@ -0,0 +1,41 @@
+import { useParams } from "react-router-dom"
+import "./index.scss"
+import { EditorContainer } from "./EditorContainer";
+export const PlaygroundScreen = () => {
+
+  const params = useParams();
+  const{fileId, folderId} = params;
+  return (
+    <div className="playground-container">
+        <div className="header-container">
+            <img src="/logo.png" className="logo"/>
+        </div>
+        <div className="content-container">
+            <div className="editor-container">
+              <EditorContainer />
+            </div>
+            <div className="input-output-container">
+                <div className="input-header">
+                  <b>Input:</b>
+                  <label htmlFor="input" className="icon-container">
+                      <span className="material-icons">cloud_upload</span>
+                      <b className="">Import Input</b>
+                  </label>
+                  <input type="file" id="input" style={{display: 'none'}}/>
+                </div>
+                <textarea></textarea>
+            </div>
+            <div className="input-output-container">
+            <div className="input-header">
+                  <b>Output:</b>
+                  <button className="icon-container">
+                    <span className="material-icons">cloud_download</span>
+                    <b>Export Output</b>
+                  </button>
+                </div>
+                <textarea readOnly></textarea>
+            </div>
+        </div>
+    </div>
+  );
+}
